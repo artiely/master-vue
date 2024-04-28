68812d673feed208ee28f08ed52bc939d68ae54b
tanjie-ola <tan.jie@olaola.chat>
2024-04-28 10:08:26 +0800

    feat: add electron

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
 
 export default antfu(
   {
     unocss: true,
     formatters: true,
   },
+  ...compat.config({
+    // extends: [
+    //   'eslint:recommended',
+    //   // Other extends...
+    // ],
+    rules: {
+      'node/prefer-global/process': 'off',
+    },
+    env: {
+      node: true,
+    },
+  }),
 )
--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ import Shiki from '@shikijs/markdown-it'
 import WebfontDownload from 'vite-plugin-webfont-dl'
 import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
+import electron from 'vite-plugin-electron/simple'
 
 export default defineConfig({
   resolve: {
@@ -41,6 +42,18 @@ export default defineConfig({
 
     // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
     Layouts(),
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`
+        entry: 'electron/main.ts',
+      },
+      // preload: {
+      //   // Shortcut of `build.rollupOptions.input`
+      //   input: 'electron/preload.ts',
+      // },
+      // Optional: Use Node.js API in the Renderer process
+      renderer: {},
+    }),
 
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
